d28284856632d63e578469dbdfadd9c07ede9c29
xKomaar <jakubo148@gmail.com>
2023-11-19 17:23:13 +0100

    initial commit

---
+++ src/main/java/com/example/whattoeat_spring/WhatToEatSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.whattoeat_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatToEatSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatToEatSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/whattoeat_spring/WhatToEatSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.whattoeat_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatToEatSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
